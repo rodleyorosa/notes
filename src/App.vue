1eeff7be93e8810541160426bca714057c38a082
rodleyorosa <rodleyorosa@gmail.com>
2023-02-10 12:09:29 +0100

    add title

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,35 @@
     v-if="isModalOpened"
     @cancelModal="cancelModal()"
     @addNote="addNote" />
-    <div class="max-w-md m-auto flex justify-between items-center">
-      <h1 class="text-4xl font-bold">NOTES</h1>
-      <button
-      @click="isModalOpened = true"
-      class="bg-gray-800 text-white w-10 h-10 rounded-full hover:bg-gray-900">+</button>
+    <ModalDelete
+    v-if="isDeleteModalOpened"
+    @deleteNote="deleteNote"
+    @cancelDelete="isDeleteModalOpened = false" />
+    <div class="max-w-md m-auto">
+      <div class="flex justify-between items-center">
+        <h1 class="text-4xl font-bold">NOTES</h1>
+        <button
+        @click="openModal()"
+        class="bg-gray-800 text-white w-10 h-10 rounded-full hover:bg-gray-900">+</button>
+      </div>
+      <p class="text-sm">* Done with emit</p>
     </div>
-    <div class="max-w-4xl m-auto py-10 flex flex-wrap justify-around">
-      <div v-for="note in notes" :key="note">
-        <div class="w-56 h-56 bg-orange-300 p-2.5 rounded-xl flex flex-col justify-between mr-5 mb-5">
-          <p class="">{{ note.text }}</p>
+    <div class="max-w-6xl m-auto py-10 flex flex-wrap justify-around">
+      <div v-for="(note, id) in notes" :key="id">
+        <div
+        class="w-56 h-56 p-2.5 overflow-hidden rounded-xl flex flex-col justify-between mr-5 mb-5"
+        :style="{backgroundColor: note.bgColor}">
+          <div>
+            <div class="flex justify-between items-center">
+              <h2 class="font-bold">{{ note.newNote.title }}</h2>
+              <button @click.prevent="isDeleteModalOpened = true">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+                </svg>
+              </button>
+            </div>
+            <p class="">{{ note.newNote.msg }}</p>
+          </div>
           <p class="text-xs font-bold">{{ note.date }}</p>
         </div>
       </div>
@@ -23,30 +42,54 @@
 
 <script setup>
 import { ref } from 'vue'
-import Modal from '../src/components/Modal.vue'
+import Modal from './components/Modal.vue'
+import ModalDelete from './components/ModalDelete.vue'
 
 const isModalOpened = ref(false)
 const notes = ref([])
+const textarea = ref(null)
+const isDeleteModalOpened = ref(false)
+
+function getRandomColor() {
+  return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+}
+
+function openModal() {
+  isModalOpened.value = true
+}
 
 function cancelModal() {
   isModalOpened.value = false
 }
 
-function addNote(newNote) {
+function addNote(title) {
   let objectDate = new Date();
   let day = objectDate.getDate();
-  let month = objectDate.getMonth();
+  if (day < 10) {
+    day = '0' + day
+  }
+  let month = objectDate.getMonth() + 1;
+  if (month < 10) {
+    month = '0' + month
+  }
   let year = objectDate.getFullYear();
   let date = day + '/' + month + '/' + year
 
   notes.value.push({
-    id: Math.floor(Math.random() * 1000000),
-    text: newNote,
+    // id: Math.floor(Math.random() * 1000000),
+    newNote: title,
     date: date,
-    // bgColor: ,
+    bgColor: getRandomColor(),
   })
+  console.log(notes.value)
 
   isModalOpened.value = false
+}
+
+function deleteNote(id) {
+    // notes.value.splice(id, 1)
+    // isDeleteModalOpened.value = false
+    console.log(id)
 }
 
 </script>
--- src/components/ModalDelete.vue
+++ src/components/ModalDelete.vue
@@ -6,32 +6,22 @@
           <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
             <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <div class="sm:flex sm:items-start">
-                <div class="mt-3 text-center w-full sm:mt-0 sm:text-left space-y-4">
-                  <input
-                  placeholder="Title"
-                  v-model.trim="title"
-                  maxlength=20
-                  class="w-full p-2 border rounded shadow-md"
-                  />
-                  <textarea name="text" id="text" rows="4"
-                  placeholder="Write something ..."
-                  class="w-full p-2 border rounded shadow-md"
-                  v-model.trim="msg"
-                  ></textarea>
+                <div class="mt-3 text-center w-full sm:mt-0 sm:text-left">
+                 <h2 class="font-bold text-2xl text-red-500">DELETE</h2>
+                 <p>Are you sure to delete this note?</p>
                 </div>
               </div>
             </div>
             <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
               <button type="button"
-              :disabled="!msg"
-              :class="!msg ? 'bg-gray-300 hover:bg-gray-300' : ''"
-              class="inline-flex w-full justify-center rounded-md border border-transparent bg-blue-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
-              @click.prevent="$emit('addNote', {title, msg})"
-              >Send</button>
+              class="inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:w-auto sm:text-sm"
+              @click.prevent="$emit('deleteNote')"
+              >Delete</button>
               <button type="button"
               class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-              @click.prevent="$emit('cancelModal')"
+              @click.prevent="$emit('cancelDelete')"
               >Cancel</button>
+              {{ id }}
             </div>
           </div>
         </div>
@@ -40,9 +30,4 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
-
-const title = ref("")
-const msg = ref("")
-
 </script>
